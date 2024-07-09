bb4a8ea57ae01f8598c6e774ff0fe268d4135fc7
amirHossein5 <amir5665@yahoo.com>
2024-07-09 23:01:13 +0330

    Add basic mpc parsing

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "mpc.h"
 
 #ifdef _WIN32
 
@@ -25,6 +26,20 @@ void add_history(char* input);
 #include <editline/history.h>
 
 int main() {
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Lispy = mpc_new("lispy");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+              " \
+number  : /-?[0-9]+/ ; \
+operator: '+' | '-' | '*' | '/' ; \
+expr: <number> | '(' <operator> <expr>+ ')' ; \
+lispy: /^/ <operator> <expr>+ /$/ ; \
+",
+              Number, Operator, Expr, Lispy);
+
     puts("Lispy version 0.0.1");
     puts("Press Ctrl+c to exit\n");
 
@@ -33,7 +48,19 @@ int main() {
 
         add_history(input);
 
+        mpc_result_t r;
+        if(mpc_parse("<stdin>", input, Lispy, &r)) {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
+        free(input);
     }
+
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
     return 0;
 }
