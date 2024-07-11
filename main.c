e7c1e7b8d7c5f9aed16eaf74508d4a7839fe8e04
amirHossein5 <amir5665@yahoo.com>
2024-07-11 17:48:49 +0330

    Add min max operators

--- main.c
+++ main.c
@@ -32,6 +32,8 @@ long eval_operation(char* operator, long n1, long n2) {
     if (strcmp(operator, "/") == 0) { return n1 / n2; }
     if (strcmp(operator, "^") == 0) { return pow(n1, n2); }
     if (strcmp(operator, "%") == 0) { return n1 % n2; }
+    if (strcmp(operator, "min") == 0) { return n1 < n2 ? n1 : n2; }
+    if (strcmp(operator, "max") == 0) { return n1 > n2 ? n1 : n2; }
 
     return 0;
 }
@@ -60,7 +62,7 @@ int main() {
     mpca_lang(MPCA_LANG_DEFAULT,
               " \
 number  : /-?[0-9]+/ ; \
-operator: '+' | '-' | '*' | '/' | '^' | '%' ; \
+operator: '+' | '-' | '*' | '/' | '^' | '%' | \"min\" | \"max\" ; \
 expr: <number> | '(' <operator> <expr>+ ')' ; \
 lispy: /^/ <operator> <expr>+ /$/ ; \
 ",
